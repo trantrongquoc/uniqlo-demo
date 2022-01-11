cd988d7350d4d3518777fcfbb59b2512f3590eb9
quoctran <trantrongquoc@gmail.com>
2022-01-12 00:18:14 +0700

    delete unnecessary file

--- java/javaOOP/Extend_Default_Class.java
+++
@@ -1,6 +0,0 @@
-package javaOOP;
-
-public class Extend_Default_Class {
-	// Khác package thì không thể extend một default class
-	// public class Extend_Default_Class extends Motobike sẽ báo lỗi
-}
